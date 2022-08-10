0cac9458b19e030475c4b8bf693b36d61894028e
josemanuel <g.josemanuel@ext.douglas.eu>
2022-08-10 03:29:24 +0200

    add flac support, change min threshold, isolate decode function to retrieve error

--- audioclassifier.js
+++ audioclassifier.js
@@ -4,14 +4,15 @@ import fetch from 'node-fetch'
 import decode from 'audio-decode'
 /* https://github.com/audiocogs/vorbis.js#node-usage*/
 import * as AV from 'vorbis.js'
-import * as ogg from 'ogg.js'
+import flac from 'flac.js'
+//import * as ogg from 'ogg.js'
 /*https://github.com/audiocogs/opus.js#node-usage*/
 import * as opus from 'opus.js'
 import { EssentiaWASM, EssentiaModel } from 'essentia.js'
 
 import getFinalPredResult from './audioclassifier_helper.js'
 // DOWN LIMIT TO CONSIDER A LABEL/CLASS
-export const THRESHOLD = 0.2
+export const THRESHOLD = 0.5
 
 /* MagnaTagATune */
 export const MUSICNNLABELS = ['guitar', 'classical', 'slow', 'techno', 'strings', 'drums', 'electronic', 'rock', 'fast', 'piano', 'ambient', 'beat', 'violin', 'vocal', 'synth', 'female', 'indian', 'opera', 'male', 'singing', 'vocals', 'no vocals', 'harpsichord', 'loud', 'quiet', 'flute', 'woman', 'male vocal', 'no vocal', 'pop', 'soft', 'sitar', 'solo', 'man', 'classic', 'choir', 'voice', 'new age', 'dance', 'male voice', 'female vocal', 'beats', 'harp', 'cello', 'no voice', 'weird', 'country', 'metal', 'female voice', 'choral']
@@ -20,7 +21,7 @@ let extractor = null
 let musicnn = null
 
 const isAudio = (url) => {
-  return /\.(wav|ogg|oga|mp3)$/.test(url)
+  return /\.(wav|ogg|oga|flac|mp3)$/.test(url)
 }
 
 class AudioClassifier {
@@ -63,19 +64,33 @@ class AudioClassifier {
 
     return Promise.reject({ message: 'Loading model' })
   }
-
-  async loadAudioAndDecode(audioPath) {
+  async doDecode(thebuffer){
+    console.log('start decoding')
+    let result = null
+    await decode(thebuffer).then(decoAudio => {      
+      result = decoAudio
+    }, err => {
+      console.log(err)      
+    });
+    return result
+  }
+  async loadAudioAndDecode(audioPath) {    
     const response = await fetch(audioPath)
     const audioBuffer = await response.arrayBuffer()
-    const decodedAudio = await decode(audioBuffer)
-    const audioArray = decodedAudio._data
-    return audioArray
+    const decodedAudio = await this.doDecode(audioBuffer)
+    if(!decodedAudio){     
+      return {error:"Decode error"}
+    } else{
+      const audioArray = decodedAudio._data
+      return audioArray
+    }
+    
   }
-
   async analyse(buffer) {
     const featuresStart = Date.now()
     const features = await extractor.computeFrameWise(buffer, 256)
     const extractorTime = Date.now() - featuresStart
+    console.log('start prediction')
     //console.log('computeFeatures: ', features.melSpectrum);  
     const predictioStart = Date.now()
     const predictions = await musicnn.predict(features, true)
@@ -102,11 +117,15 @@ class AudioClassifier {
   }
   async inference({ audioUrl }) {
     try {
-      const audio = await this.loadAudioAndDecode(audioUrl)
-      let analysisOutput = await this.analyse(audio)
-      const labels = analysisOutput.labels
-      analysisOutput.labels = getFinalPredResult(labels)
-      return analysisOutput
+      const audio = await this.loadAudioAndDecode(audioUrl)      
+      if(audio.error) {
+        return Promise.reject({ error:'Decode error' })
+      } else{
+        let analysisOutput = await this.analyse(audio)
+        const labels = analysisOutput.labels
+        analysisOutput.labels = getFinalPredResult(labels)
+        return analysisOutput
+      }    
     } catch (error) {
       return Promise.reject({ error })
     }
